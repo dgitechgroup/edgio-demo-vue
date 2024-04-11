0c25a055fc6dc7654b92f7c9b57e48cb8d79a8aa
yuri-dgi <157486942+yuri-dgi@users.noreply.github.com>
2024-04-11 19:58:38 +0400

    Update routes.ts

--- routes.ts
+++ routes.ts
@@ -18,7 +18,8 @@ const router = new Router()
 router.get('/gs2c/(.*)', {
   caching: {
     max_age: '1d',
-    stale_while_revalidate: '1d'
+    stale_while_revalidate: '1d',
+    first_hit_caching: true
   },
   origin: {
     set_origin: 'dgi-lab',
